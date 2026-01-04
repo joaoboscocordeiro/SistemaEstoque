f63082d925d26a7b57c8dd5c7ea0f5fdedb0d6b9
João Bosco Cordeiro de Lima Júnior <ti.junior@gmail.com>
2026-01-04 15:32:43 -0300

    Criação da View Register

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaEstoque.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register() => View();
+    }
+}
---
+++ Dtos/RegisterUserDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaEstoque.Dtos
+{
+    public class RegisterUserDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Senha { get; set; } = string.Empty;
+        [Required]
+        public string ConfirmaSenha { get; set; } = string.Empty;
+        [Required]
+        public string Perfil { get; set; } = string.Empty;
+    }
+}
