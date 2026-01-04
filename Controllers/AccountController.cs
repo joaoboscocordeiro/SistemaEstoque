7236ae36af87ab83c861eefa2fd4d7e6d7200a23
João Bosco Cordeiro de Lima Júnior <ti.junior@gmail.com>
2026-01-04 15:31:20 -0300

    Criação do Méyodo Register

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,14 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SistemaEstoque.Dtos;
 
 namespace SistemaEstoque.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public AccountController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {    
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         public IActionResult Register() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterUserDto registerUserDto)
+        {
+            if(!ModelState.IsValid) return View(registerUserDto);
+
+            var user = new IdentityUser { UserName = registerUserDto.Email, Email = registerUserDto.Email };
+
+            var result = await _userManager.CreateAsync(user, registerUserDto.Senha);
+
+            if(result.Succeeded)
+            {
+                if(!await _roleManager.RoleExistsAsync(registerUserDto.Perfil))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(registerUserDto.Perfil));
+                }
+
+                await _userManager.AddToRoleAsync(user, registerUserDto.Perfil);
+            }
+
+            TempData["MensagemSucesso"] = $"Usuário: {user.UserName} Criado com sucesso!";
+            return View();
+        }
     }
 }
--- Dtos/RegisterUserDto.cs
+++ Dtos/RegisterUserDto.cs
@@ -5,12 +5,13 @@ namespace SistemaEstoque.Dtos
 {
     public class RegisterUserDto
     {
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "Email é obrigatório!")]
+        [EmailAddress(ErrorMessage = "Email inválido!")]
         public string Email { get; set; } = string.Empty;
-        [Required]
+        [Required(ErrorMessage = "A senha é obrigatório!")]
         public string Senha { get; set; } = string.Empty;
-        [Required]
+        [Required(ErrorMessage = "A confirmação de senha é obrigatória!")]
+        [Compare("Senha", ErrorMessage = "As senhas não estão iguais!")]
         public string ConfirmaSenha { get; set; } = string.Empty;
         [Required]
         public string Perfil { get; set; } = string.Empty;
