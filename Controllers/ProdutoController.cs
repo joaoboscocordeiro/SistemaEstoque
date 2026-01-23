2ce18985d241287096f632af954567289315d8a9
João Bosco Cordeiro de Lima Júnior <ti.junior@gmail.com>
2026-01-23 19:10:44 -0300

    Criando view produto

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaEstoque.Services.Categorias;
+using SistemaEstoque.Services.Produtos;
 
 namespace SistemaEstoque.Controllers
 {
     public class ProdutoController : Controller
     {
-        public IActionResult Index()
+        private readonly IProdutoInterface _produtoInterface;
+        private readonly ICategoriaInterface _categoriaInterface;
+
+        public ProdutoController(IProdutoInterface produtoInterface, ICategoriaInterface categoriaInterface)
         {
-            return View();
+            _produtoInterface = produtoInterface;
+            _categoriaInterface = categoriaInterface;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var produtos = await _produtoInterface.ObterTodosAsync();
+            return View(produtos);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var produto = await _produtoInterface.ObterPorIdAsync(id);
+            if (produto == null) return NotFound();
+
+            return View(produto);
         }
     }
 }
