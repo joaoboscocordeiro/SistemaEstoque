72ec82dae2c766801f17f64e7bf516352abe4d61
João Bosco Cordeiro de Lima Júnior <ti.junior@gmail.com>
2026-02-11 13:25:18 -0300

    Criando editar produtos

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using SistemaEstoque.Dtos.Produto;
+using SistemaEstoque.Models;
 using SistemaEstoque.Services.Categorias;
 using SistemaEstoque.Services.Produtos;
 
@@ -43,6 +44,34 @@ namespace SistemaEstoque.Controllers
             return View(produtoDto);
         }
 
+        public async Task<IActionResult> Edit(int id)
+        {
+            var produto = await _produtoInterface.ObterPorIdAsync(id);
+            var categorias = await _categoriaInterface.ObterTodasCategorias();
+
+            if (produto == null)
+            {
+                TempData["MensagemErro"] = "Produto não encontrado!";
+                return RedirectToAction("Index");
+            }
+            ;
+
+            var produtoDto = new ProdutoDto
+            {
+                Id = id,
+                Nome = produto.Nome,
+                Descricao = produto.Descricao,
+                Preco = produto.Preco,
+                Quantidade = produto.Quantidade,
+                Codigo = produto.Codigo,
+                CategoriaId = produto.CategoriaId,
+                Imagem = produto.Imagem,
+                Categorias = new SelectList(categorias, "Id", "Descricao", produto.CategoriaId)
+            };
+
+            return View(produtoDto);
+        }
+
         [HttpPost]
         public async Task<IActionResult> Create(ProdutoDto produtoDto)
         {
@@ -64,6 +93,30 @@ namespace SistemaEstoque.Controllers
             }
 
             produtoDto.Categorias = new SelectList(categorias, "Id", "Descricao");
+            return View(produtoDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProdutoDto produtoDto)
+        {
+            var categorias = await _categoriaInterface.ObterTodasCategorias();
+
+            if (ModelState.IsValid)
+            {
+                var response = await _produtoInterface.AtualizarAsync(produtoDto);
+
+                if (!response.Status)
+                {
+                    TempData["MensagemErro"] = response.Mensagem;
+                    produtoDto.Categorias = new SelectList(categorias, "Id", "Descricao", produtoDto.CategoriaId);
+                    return View(produtoDto);
+                }
+
+                TempData["MensagemSucesso"] = response.Mensagem;
+                return RedirectToAction("Index");
+            }
+
+            produtoDto.Categorias = new SelectList(categorias, "Id", "Descricao", produtoDto.CategoriaId);
             return View(produtoDto);
         }
     }
