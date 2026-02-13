cc236ddae65a086b0e4bfa1a554b9da8854772aa
João Bosco Cordeiro de Lima Júnior <ti.junior@gmail.com>
2026-02-13 17:59:53 -0300

    Criação do Dashboard

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,46 @@
 using Microsoft.AspNetCore.Mvc;
+using SistemaEstoque.Services.Produtos;
+using System.Threading.Tasks;
 
 namespace SistemaEstoque.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IProdutoInterface _produtoInterface;
+
+        public HomeController(IProdutoInterface produtoInterface) 
         {
-            return View();
+            _produtoInterface = produtoInterface;
+        }
+
+        public async Task<IActionResult> Index(string termoBusca = "", int page = 1)
+        {
+            var produtos = await _produtoInterface.ObterTodosAsync();
+
+            var estoqueBaixo = produtos.Where(p => p.Quantidade <= 5).ToList();
+            var totalProdutos = produtos.Count();
+            var valorTotalEstoque = produtos.Sum(p => (p.Preco ?? 0) * p.Quantidade);
+
+            if (!string.IsNullOrEmpty(termoBusca))
+            {
+                produtos = produtos.Where(p => p.Nome.Contains(termoBusca, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            int pageSize = 5;
+            var produtosPaginados = produtos
+                .OrderBy(p => p.Nome)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            ViewBag.TotalProdutos = totalProdutos;
+            ViewBag.ValorTotalEstoque = valorTotalEstoque;
+            ViewBag.TotalEstoqueBaixo = estoqueBaixo.Count();
+            ViewBag.TermoBusca = termoBusca;
+            ViewBag.PaginaAtual = page;
+            ViewBag.TotalPaginas = (int)Math.Ceiling((double)produtos.Count / pageSize);
+
+            return View(produtosPaginados);
         }
     }
 }
